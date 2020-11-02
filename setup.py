baa57e9c0cb9b4055c3db5ca9a6b525203e8af70
Adrian Sharpe <asharpe@nationaljournal.com>
2020-11-02 10:53:07 -0500

    requirements.txt changes
    undo requirements.txt change
    
    Revert "undo requirements.txt change"
    
    This reverts commit dcbfb94bedd1ca39d74524a06a6d39c816b3da78.

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
         "CacheControl==0.12.*",
         "cement==2.10.2",
         "lockfile==0.12.2",
-        "pymongo==3.3",
+        "pymongo==3.6.1",
         "python-dateutil==2.7.*",
-        "requests==2.20.*",
+        "requests==2.22.*",
         "ujson==1.35",
     ],
     classifiers=[
