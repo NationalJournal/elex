1270db01200036fe226fd8a8e694de5da7da546f
Vitaliy Utrobin <vutrobin@nationaljournal.com>
2023-12-01 11:37:19 -0500

    changed print statement to print to standard output, not error.

--- elex/api/utils.py
+++ elex/api/utils.py
@@ -91,7 +91,7 @@ def api_request(path, **params):
     params = sorted(params.items())  # Sort for consistent caching
 
     url = "{0}{1}".format(elex.BASE_URL, path.replace("//", "/"))
-    print("Requesting {0} from {1}".format(params, url), file=sys.stderr)
+    print("Requesting {0} from {1}".format(params, url))
 
     response = cache.get(url, params=params)
     response.raise_for_status()
